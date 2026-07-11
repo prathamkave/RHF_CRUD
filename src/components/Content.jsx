23359788ed3f503ad669408e06bdee80ec2b84ee
Mannsoni8 <mannsoni4352@gmail.com>
2026-07-11 10:50:03 +0530

    gap increase

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,53 +5,55 @@ import Form from "./Form";
 import UserCard from "./UserCard";
 
 const Content = () => {
-
   const [toggle, setToggle] = useState(false);
-	const [users, setUsers] = useState(() => {
-		return JSON.parse(localStorage.getItem("users")) || [];
-	});
+  const [users, setUsers] = useState(() => {
+    return JSON.parse(localStorage.getItem("users")) || [];
+  });
   const [updatedData, setUpdatedData] = useState(null);
   const [search, setSearch] = useState("");
 
-	const filteredUsers = users.filter((user) => {
-		return (
-			user.name.toLowerCase().includes(search.toLowerCase()) ||
-			user.email.toLowerCase().includes(search.toLowerCase()) ||
-			user.role.toLowerCase().includes(search.toLowerCase()) ||
-			user.phone.includes(search)
-		);
-	});
+  const filteredUsers = users.filter((user) => {
+    return (
+      user.name.toLowerCase().includes(search.toLowerCase()) ||
+      user.role.toLowerCase().includes(search.toLowerCase())
+    );
+  });
 
-	const deleteUser = (id) => {
-		const filteredUsers = users.filter((user) => user.id !== id);
+  const deleteUser = (id) => {
+    const filteredUsers = users.filter((user) => user.id !== id);
 
-		setUsers(filteredUsers);
-		localStorage.setItem("users", JSON.stringify(filteredUsers));
-	};
+    setUsers(filteredUsers);
+    localStorage.setItem("users", JSON.stringify(filteredUsers));
+  };
 
   return (
-		<div className='px-36 h-[calc(100vh-150px)] bg-[#24221B]'>
-			{toggle && (
-				<Form
-        setToggle={setToggle}
-        setUsers={setUsers}
-        users={users}
-        updatedData={updatedData}
-				/>
-			)}
+    <div className="px-36 h-[calc(100vh-150px)] bg-[#24221B]">
+      {toggle && (
+        <Form
+          setToggle={setToggle}
+          setUsers={setUsers}
+          users={users}
+          updatedData={updatedData}
+        />
+      )}
 
-			<div className='mt-4'>
-				<SearchBar setToggle={setToggle} setUpdatedData={setUpdatedData} search={search} setSearch={setSearch} />
-			</div>
+      <div className="mt-4">
+        <SearchBar
+          setToggle={setToggle}
+          setUpdatedData={setUpdatedData}
+          search={search}
+          setSearch={setSearch}
+        />
+      </div>
 
-			<UserCard
-				setUpdatedData={setUpdatedData}
-				users={filteredUsers}
-				deleteUser={deleteUser}
+      <UserCard
+        setUpdatedData={setUpdatedData}
+        users={filteredUsers}
+        deleteUser={deleteUser}
         setToggle={setToggle}
-			/>
-		</div>
-	);
+      />
+    </div>
+  );
 };
 
 export default Content;
